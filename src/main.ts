9f08736a392f2ab3010996d8bffc5df143ea0b12
denzquix <denzquix@gmail.com>
2025-04-11 16:10:17 +0100

    Update interface to include more options

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,89 @@
 import { FileMap, makeFileReceiver } from "./drag-drop";
 import { VFSVolume, VFSDirectoryEntry } from "./virtual-file-system";
 
+function getTabContext(el: Element) {
+  for (let ancestor = el.parentElement; ancestor; ancestor = ancestor.parentElement) {
+    if (ancestor.hasAttribute('data-tab-context')) return ancestor;
+  }
+  return null;
+}
+
 window.addEventListener('DOMContentLoaded', function() {
   try {
-    const inputPane = document.querySelector<HTMLElement>('.input-pane');
-    if (!inputPane) throw new Error('input-pane not found');
-    const inputPaneBody = inputPane.querySelector<HTMLElement>('.pane-body');
-    if (!inputPaneBody) throw new Error('input pane body not found');
-    const inputAddButton = inputPane.querySelector<HTMLElement>('.pane-actions > .add-files');
-    const volume = new VFSVolume();
-    const entryToElement = new Map<VFSDirectoryEntry, HTMLElement>();
-    entryToElement.set(volume.root, inputPaneBody);
-    volume.events.subscribe(e => {
-      switch (e.type) {
-        case 'directory-created': {
-          const parentEl = entryToElement.get(e.directory.parentDirectory);
-          if (parentEl) {
-            const folderContainer = document.createElement('div');
-            folderContainer.classList.add('folder');
-            const folderTitle = document.createElement('div');
-            folderTitle.classList.add('title');
-            folderTitle.textContent = e.directory.name + '/';
-            folderContainer.appendChild(folderTitle);
-            const folderContents = document.createElement('div');
-            folderContents.classList.add('contents');
-            entryToElement.set(e.directory, folderContents);
-            folderContainer.appendChild(folderContents);
-            parentEl.appendChild(folderContainer);
-          }
-          else {
-            console.warn('No element found for ' + e.directory.parentDirectory.getPath().join('/'));
-          }
-          break;
+
+    document.querySelectorAll<HTMLElement>('[data-tab-context]').forEach(tabContext => {
+      tabContext.addEventListener('click', e => {
+        const targetEl = e.target as HTMLElement;
+        if (targetEl.hasAttribute('data-tab')) {
+          e.stopPropagation();
+          tabContext.querySelectorAll<HTMLElement>('[data-tab]').forEach(tabButton => {
+            if (getTabContext(tabButton) !== tabContext) return;
+            tabButton.classList.toggle('active', tabButton.dataset.tab === targetEl.dataset.tab);
+          });
+          tabContext.querySelectorAll<HTMLElement>('[data-tab-content]').forEach(tabContent => {
+            if (getTabContext(tabContent) !== tabContext) return;
+            tabContent.classList.toggle('active', tabContent.dataset.tabContent === targetEl.dataset.tab);
+          });
         }
-        case 'file-created': {
-          const dirEl = entryToElement.get(e.file.parentDirectory);
-          if (dirEl) {
-            const fileContainer = this.document.createElement('div');
-            entryToElement.set(e.file, fileContainer);
-            fileContainer.classList.add('file');
-            const fileTitle = document.createElement('div');
-            fileTitle.classList.add('title');
-            fileTitle.textContent = e.file.name;
-            fileContainer.appendChild(fileTitle);
-            dirEl.appendChild(fileContainer);
+      });
+    });
+
+    document.querySelectorAll<HTMLElement>('.dropzone').forEach(dropzone => {
+      const targetVolume = dropzone.dataset.targetVolume;
+      if (!targetVolume) return;
+      const targetVolumeContainer = document.querySelector<HTMLElement>('[data-volume="'+targetVolume+'"]');
+      if (!targetVolumeContainer) return;
+      const volume = new VFSVolume();
+      const entryToElement = new Map<VFSDirectoryEntry, HTMLElement>();
+      entryToElement.set(volume.root, targetVolumeContainer);
+      volume.events.subscribe(e => {
+        switch (e.type) {
+          case 'directory-created': {
+            const parentEl = entryToElement.get(e.directory.parentDirectory);
+            if (parentEl) {
+              const folderContainer = document.createElement('div');
+              folderContainer.classList.add('tree-item', 'folder');
+              const folderTitle = document.createElement('div');
+              folderTitle.classList.add('title');
+              folderTitle.textContent = e.directory.name + '/';
+              folderContainer.appendChild(folderTitle);
+              const folderContents = document.createElement('div');
+              folderContents.classList.add('tree-children', 'expanded');
+              entryToElement.set(e.directory, folderContents);
+              folderContainer.appendChild(folderContents);
+              parentEl.appendChild(folderContainer);
+            }
+            else {
+              console.warn('No element found for ' + e.directory.parentDirectory.getPath().join('/'));
+            }
+            break;
           }
-          else {
-            console.warn('No element found for ' + e.file.parentDirectory.getPath().join('/'));
+          case 'file-created': {
+            const dirEl = entryToElement.get(e.file.parentDirectory);
+            if (dirEl) {
+              const fileContainer = this.document.createElement('div');
+              entryToElement.set(e.file, fileContainer);
+              fileContainer.classList.add('tree-item', 'file');
+              const fileTitle = document.createElement('div');
+              fileTitle.classList.add('title');
+              fileTitle.textContent = e.file.name;
+              fileContainer.appendChild(fileTitle);
+              dirEl.appendChild(fileContainer);
+            }
+            else {
+              console.warn('No element found for ' + e.file.parentDirectory.getPath().join('/'));
+            }
+            break;
           }
-          break;
         }
-      }
+      });
+      makeFileReceiver({
+        dropTarget: dropzone,
+        button: dropzone,
+        targetDirectory: volume.root,
+      });
     });
-    makeFileReceiver({
-      dropTarget: inputPane,
-      button: inputAddButton,
-      targetDirectory: volume.root,
-    });
+
   }
   catch (e) {
     console.error(e);
